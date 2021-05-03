02789bac7ae1a93d61445adbaf463ddae12eea57
jorge-datanalyst <jorge.guevara@quantil.com.co>
2021-05-03 07:02:58 -0500

    Se crea App Experience con todas sus dependencias como modelo, vistas, template y demas...

--- Cv_Personal/Experience/views.py
+++ Cv_Personal/Experience/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-#from Certificados.models import CertificadosModel
+from Experience.models import ExperienceModel
 from django.views.generic import (TemplateView, ListView)
 
 #Create your views here.
-class ExperienceView(TemplateView):
-    #model = ExperienceModel
+class ExperienceView(ListView):
+    model = ExperienceModel
     context_object_name = 'experienceview'
     template_name = 'Experience/experience.html'
